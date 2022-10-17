b637f87677c83387bad728cf500c1287038172a6
trungnguyenhuynhminh46 <trungnguyenhuynhminh46@gmail.com>
2022-10-17 10:24:57 +0700

    Fix something

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useRef } from "react";
+import { formatRelative } from "date-fns/esm";
 // Assets
 import AuthContext from "../../context/AuthContext";
 import ChatContext from "../../context/ChatContext";
 
 const Message = ({ message }) => {
+  // console.log(message.date.toDate());
   const ref = useRef();
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
@@ -11,6 +13,19 @@ const Message = ({ message }) => {
   useEffect(() => {
     ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
+
+  function formatDate(seconds) {
+    let formattedDate = "";
+
+    if (seconds) {
+      formattedDate = formatRelative(new Date(seconds * 1000), new Date());
+
+      formattedDate =
+        formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    }
+
+    return formattedDate;
+  }
   return (
     <div
       ref={ref}
@@ -20,10 +35,10 @@ const Message = ({ message }) => {
     >
       <div className="info">
         <img src={data.user.photoURL} alt="" className="thumb" />
-        <span>Just now</span>
+        <span>{message.date && formatDate(message.date.seconds)}</span>
       </div>
       <div className="message-content">
-        <span>{message.text}</span>
+        {message.text && <span>{message.text}</span>}
         {message.image && <img src={message.image} alt="" />}
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+// Assets
+import ChatContext from "../context/ChatContext";
 // Components
 import Chat from "../components/Chat";
 import Sidebar from "../components/Sidebar";
 
 const Home = () => {
+  const { data } = useContext(ChatContext);
   return (
     <div className="flex justify-center">
       <div className="flex w-[75%] h-[600px] rounded-lg overflow-hidden shadow-lg">
         <Sidebar></Sidebar>
-        <Chat></Chat>
+        {data.user.uid ? (
+          <Chat></Chat>
+        ) : (
+          <div className=" basis-2/3 flex justify-center items-center bg-slate-200">
+            <span className="text-3xl text-slate-500 uppercase">
+              Chose a chat to start the conversation
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
